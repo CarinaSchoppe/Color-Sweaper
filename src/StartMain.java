d24028a1c28b69060df8dd340b3b7e9bc94bc139
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2023-06-26 12:45:44 +0200

    commit

--- src/StartMain.java
+++ src/StartMain.java
@@ -11,7 +11,7 @@ public class StartMain {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(GameWindow::createAndShowGUI);
 
-        //TODO: KI -> Greedy & stagnation -> clone the board calculate all adjecent cells and check if the move is valid
+        //TODO: KI -> Greedy & stagnation -> clone the board calculate all adjecent cells and perform a clone move. calculate the gain and choose the best move.
     }
 
 }
